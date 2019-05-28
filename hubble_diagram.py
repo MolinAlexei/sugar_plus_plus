03ea5779d3a8168334699b190c9dae57d3b3057f
MolinAlexei <molinalexei@gmail.com>
2019-05-28 14:49:26 +0200

    Hubble diagram with host properties working

--- hubble_diagram.py
+++ hubble_diagram.py
@@ -11,35 +11,65 @@ from toolbox import distance_modulus
 import matplotlib.gridspec as gridspec
 import copy
 
-def load_salt2(File):
+def load_salt2(File, File_host):
+
+    table_host = np.loadtxt(File_host, comments='#', delimiter=',', dtype='str',skiprows=1)
+    
+    sn_name_host =  table_host[:,16]
 
     dic = pickle.load(open(File))
+    sn_name_dic = dic.keys()
 
-    X1 = np.array([dic[sn]['salt2_info']['X1'] for sn in dic])
-    C = np.array([dic[sn]['salt2_info']['C'] for sn in dic])
-    mb = np.array([dic[sn]['salt2_info']['delta_mu'] for sn in dic])
-    delta_mu_err = np.array([dic[sn]['salt2_info']['delta_mu_err'] for sn in dic])
-    X1_err = np.array([dic[sn]['salt2_info']['X1_err'] for sn in dic])
-    C_err = np.array([dic[sn]['salt2_info']['C_err'] for sn in dic])
-    delta_mu_X1_cov = np.array([dic[sn]['salt2_info']['delta_mu_X1_cov'] for sn in dic])
-    delta_mu_C_cov = np.array([dic[sn]['salt2_info']['delta_mu_C_cov'] for sn in dic])
-    X1_C_cov = np.array([dic[sn]['salt2_info']['X1_C_cov'] for sn in dic])
-    dmz = np.array([dic[sn]['salt2_info']['dmz'] for sn in dic])
-    zcmb = np.array([dic[sn]['salt2_info']['zcmb'] for sn in dic])
+    sn_name = []
 
-    params = np.array([X1,C]).T
+    for sn in sn_name_host:
+        if sn in sn_name_dic:
+            sn_name.append(sn)
+    
+
+    X1 = np.array([dic[sn]['salt2_info']['X1'] for sn in sn_name])
+    C = np.array([dic[sn]['salt2_info']['C'] for sn in sn_name])
+    mb = np.array([dic[sn]['salt2_info']['delta_mu'] for sn in sn_name])
+    delta_mu_err = np.array([dic[sn]['salt2_info']['delta_mu_err'] for sn in sn_name])
+    X1_err = np.array([dic[sn]['salt2_info']['X1_err'] for sn in sn_name])
+    C_err = np.array([dic[sn]['salt2_info']['C_err'] for sn in sn_name])
+    delta_mu_X1_cov = np.array([dic[sn]['salt2_info']['delta_mu_X1_cov'] for sn in sn_name])
+    delta_mu_C_cov = np.array([dic[sn]['salt2_info']['delta_mu_C_cov'] for sn in sn_name])
+    X1_C_cov = np.array([dic[sn]['salt2_info']['X1_C_cov'] for sn in sn_name])
+    dmz = np.array([dic[sn]['salt2_info']['dmz'] for sn in sn_name])
+    zcmb = np.array([dic[sn]['salt2_info']['zcmb'] for sn in sn_name])
+
+    
     cov = np.zeros((len(X1), 3,3))
 
     for k in range(len(X1)):
-        cov[k] = np.array([[delta_mu_err[k]**2, delta_mu_X1_cov[k],delta_mu_C_cov[k]],
+        cov[k] = np.array([[delta_mu_err[k]**2, delta_mu_X1_cov[k], delta_mu_C_cov[k]],
                            [delta_mu_X1_cov[k], X1_err[k]**2,  X1_C_cov[k]],
                            [delta_mu_C_cov[k], X1_C_cov[k], C_err[k]**2]])
-    return mb, params, cov ,zcmb, dmz
+    
+
+    global_mass = np.zeros_like(X1)
+    lssfr = np.zeros_like(X1)
+    p_hightmass = np.zeros_like(X1)
+    p_prompt = np.zeros_like(X1)
+
+    count=0
+    for k,sn in enumerate(sn_name_host):
+        if sn in sn_name :
+            global_mass[count] = table_host[k,4].astype(float)
+            lssfr[count] = table_host[k,10].astype(float)
+            p_hightmass[count] = table_host[k,17].astype(float)
+            p_prompt[count] = table_host[k,18].astype(float)
+            count += 1
+
+    params = np.array([X1,C]).T
+
+    return mb, params, cov ,zcmb, dmz, global_mass, lssfr, p_hightmass, p_prompt
 
 
 class hubble_diagram(object):
 
-    def __init__(self, mb, data, cov, zcmb, dmz):
+    def __init__(self, mb, data, cov, zcmb, dmz, global_mass, lssfr, p_hightmass, p_prompt):
 
 
         self.mb = mb 
@@ -47,6 +77,10 @@ class hubble_diagram(object):
         self.cov = cov
         self.zcmb = zcmb
         self.dmz = dmz
+        self.global_mass = global_mass
+        self.lssfr = lssfr
+        self.p_hightmass = p_hightmass
+        self.p_prompt = p_prompt
 
         self.ntheta = len(self.data[0]) + 1
         self.theta = np.ones(self.ntheta)
@@ -69,7 +103,8 @@ class hubble_diagram(object):
         vec_theta = np.array(vec_theta)
         for sn in range(self.nsn):
             
-            var[sn] = np.dot(np.dot(vec_theta,self.cov[sn]), vec_theta.reshape((self.ntheta,1))) + 0.03**2+ self.sigma_int**2 + self.dmz[sn]**2
+            var[sn] = np.dot(np.dot(vec_theta,self.cov[sn]), vec_theta.reshape((self.ntheta,1))) + self.sigma_int**2 + self.dmz[sn]**2 
+#On a retire sigma_b = 0.03
 
         self.residuals = residuals
         self.var = var
@@ -91,9 +126,9 @@ class hubble_diagram(object):
         self.theta = optimize.fmin(self.comp_chi2, p0)
         c = (self.comp_chi2(self.theta) / self.dof) - 1
         count = 0
- 
-        while abs(c)>1E-2:
-            print(c)
+        print('before iteration', c+1)
+        while c>1E-2:
+            print('during_iteration', c+1)
 
             self.sigma_int = optimize.fsolve(self.chi2_dof, 0.1)
 
@@ -139,7 +174,26 @@ class hubble_diagram(object):
             plt.ylabel('$m_B - 5 \t \log_{10} d_l$')
             plt.legend(('fit $q_%i \t $%s$ + M_B$ '%(k,param_name[k]), 'Ajusted $m_B$'))
             plt.show()
+            
+            if self.global_mass[0] != None:
+                plt.scatter(self.global_mass, self.data[:,k], c=self.p_hightmass, cmap = 'bwr')
+                plt.title('Parameter %s as a function of host galaxy mass'%(param_name[k]))
+                plt.grid()
+                plt.xlabel('$\log_{10} \\frac{M_{galaxy}}{M_{\odot}}$', fontsize=14)
+                plt.ylabel('Parameter %s'%(param_name[k]))
+                cb = plt.colorbar()
+                cb.set_label('Probability of having this mass')
+                plt.show()
 
+            if self.lssfr[0] != None:
+                plt.scatter(self.lssfr, self.data[:,k], c=self.p_hightmass, cmap = 'bwr')
+                plt.title('Parameter %s as a function of local specific star formation rate'%(param_name[k]))
+                plt.grid()
+                plt.xlabel('Local sSFR', fontsize = 14)
+                plt.ylabel('Parameter %s'%(param_name[k]), fontsize = 14)
+                cb = plt.colorbar()
+                cb.set_label('Probability of having this local sSFR')
+                plt.show()
 
         z_span = np.linspace(1E-2,0.15,100)
         mu_th = distance_modulus(z_span)
@@ -186,12 +240,32 @@ class hubble_diagram(object):
         plt.subplots_adjust(hspace=0)
         plt.show()
 
+        if self.global_mass[0] != None:
+            plt.scatter(self.global_mass, self.residuals, c=self.p_hightmass, cmap = 'bwr')
+            plt.title('Hubble diagram residuals as a function of host galaxy mass')
+            plt.grid()
+            plt.xlabel('$\log_{10} \\frac{M_{galaxy}}{M_{\odot}}$', fontsize = 14)
+            plt.ylabel('$\Delta \mu$', fontsize = 14)
+            cb = plt.colorbar()
+            cb.set_label('Probability of having this mass')
+            plt.show()
+
+        if self.lssfr[0] != None:
+            plt.scatter(self.lssfr, self.data[:,k], c=self.p_hightmass, cmap = 'bwr')
+            plt.title('Hubble diagram residuals as a function of local specific star formation rate')
+            plt.grid()
+            plt.xlabel('Local sSFR', fontsize = 14)
+            plt.ylabel('$\Delta \mu$', fontsize = 14)
+            cb = plt.colorbar()
+            cb.set_label('Probability of having this local sSFR')
+            plt.show()
+
         
     
 if __name__ == "__main__":
-
-    mb, data, cov, zcmb, dmz= load_salt2('../snfactory/data_complete/sugar_companion_dataset.pkl') 
-    hd = hubble_diagram(mb, data, cov, zcmb, dmz)
+    file_host = '../snfactory/lssfr_paper_full_sntable.csv'
+    mb, data, cov, zcmb, dmz, global_mass, lssfr, p_hightmass, p_prompt= load_salt2('../snfactory/data_complete/sugar_companion_dataset.pkl', file_host) 
+    hd = hubble_diagram(mb, data, cov, zcmb, dmz, global_mass, lssfr, p_hightmass, p_prompt)
     hd.plot_results(['$X_1$', '$C$'])
 
     
