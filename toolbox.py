f6e0ce7cb6139ce885eea5e17fb24d6f4345d4c5
MolinAlexei <molinalexei@gmail.com>
2019-05-29 10:37:50 +0200

    toolbox with weighted std

--- toolbox.py
+++ toolbox.py
@@ -13,6 +13,15 @@ def luminosity_distance(z):
 def distance_modulus(z):
     return 5. * np.log10(luminosity_distance(z)) - 5.
 
+def weighted_avg_and_std(values, weights):
+    """
+    Return the weighted average and standard deviation.
+
+    values, weights -- Numpy ndarrays with the same shape.
+    """
+    average = np.average(values, weights=weights)
+    variance = np.average((values-average)**2, weights=weights)  # Fast and numerically precise
+    return np.sqrt(variance)
 
 if __name__ == '__main__':
 
